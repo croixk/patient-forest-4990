71903f9acebd910221e542d01e23a6bee157259f
Croix Kimmel <croixkimmel@gmail.com>
2021-12-14 13:43:05 -0700

    Update to include some of user story 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :studio
+  has_many :actors
+
+
+
+  def actor_average_age
+  end 
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'the studio index' do
+RSpec.describe 'Movies show page' do
 
-  it 'contains information about studio' do
+  it 'contains movie attributes' do
     @universal_studios = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
     @raiders = Movie.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio_id: "#{@universal_studios.id}")
-    visit '/studios'
-    expect(page).to have_content(@universal_studios.name)
-    expect(page).to have_content(@universal_studios.location)
+    visit "/movies/#{@raiders.id}"
     expect(page).to have_content(@raiders.title)
+    expect(page).to have_content(@raiders.creation_year)
+    expect(page).to have_content(@raiders.genre)
+    expect(page).to have_content(@raiders.actors)
+    expect(page).to have_content(@raiders.actor_average_age)
   end
 
 end
