2dd25ad34791bc010708c42bdefb9a46b5c60bdf
Croix Kimmel <croixkimmel@gmail.com>
2021-12-14 13:26:18 -0700

    Add complete user story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe 'the studio index' do
 
   it 'contains information about studio' do
     @universal_studios = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
-    @raiders = Movie.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio: "#{@universal_studios.id}")
+    @raiders = Movie.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio_id: "#{@universal_studios.id}")
     visit '/studios'
-    expect(page).to have_content(@studio.name)
-    expect(page).to have_content(@studio.location)
-    # save_and_open_page
+    expect(page).to have_content(@universal_studios.name)
+    expect(page).to have_content(@universal_studios.location)
+    expect(page).to have_content(@raiders.title)
+    save_and_open_page
   end
 
 end
