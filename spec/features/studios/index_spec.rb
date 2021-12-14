0833146dcce8b8b434176a45dee574d5adb5659e
Croix Kimmel <croixkimmel@gmail.com>
2021-12-14 13:14:35 -0700

    Add second half of user story one

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe 'the studio index' do
 
   it 'contains information about studio' do
-    @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    @universal_studios = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    @raiders = Movie.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio: "#{@universal_studios.id}")
     visit '/studios'
     expect(page).to have_content(@studio.name)
     expect(page).to have_content(@studio.location)
-
+    # save_and_open_page
   end
 
 end
