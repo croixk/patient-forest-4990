a22d64bc4efa6dacf654f577a87de640a0784d7b
Croix Kimmel <croixkimmel@gmail.com>
2021-12-14 15:23:50 -0700

    Add final changes

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Actor, type: :model do
 
   describe "relationships" do
     it {should have_many :movie_actors}
-    it {should have_many :movies, through: :movie_actors}
+    it {should have_many(:movies).through(:movie_actors)}
 
   end
 
